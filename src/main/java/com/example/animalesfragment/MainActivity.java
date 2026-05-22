09b238a4d48e7c864a96cdbbc9d10d77dd3857b3
Luis Ricardo <luisvega89@aragon.unam.mx>
2026-05-22 10:16:59 -0600

    Add files via upload

---
+++ src/main/java/com/example/animalesfragment/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.animalesfragment;
+/// LUIS RICARDO VEGA HERNANDEZ
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lista;
+
+    String[] animales = {
+            "Gato",
+            "Perro",
+            "León"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lista = findViewById(R.id.listaAnimales);
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        animales
+                );
+
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener((parent, view, position, id) -> {
+
+            Fragment fragment = null;
+
+            switch(position){
+
+                case 0:
+                    fragment = new GatoFragment();
+                    break;
+
+                case 1:
+                    fragment = new PerroFragment();
+                    break;
+
+                case 2:
+                    fragment = new VacaFragment();
+                    break;
+            }
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(
+                            R.id.contenedorFragment,
+                            fragment
+                    )
+                    .commit();
+        });
+    }
+}
+
+/// LUIS RICARDO VEGA HERNANDEZ
---
+++ src/main/java/com/example/animalesfragment/VacaFragment.java
@@ -0,0 +1,25 @@
+package com.example.animalesfragment;
+/// LUIS RICARDO VEGA HERNANDEZ
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VacaFragment extends Fragment {
+
+    public VacaFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(
+                R.layout.fragment_vaca,
+                container,
+                false
+        );
+    }
+}
